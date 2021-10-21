5d2c422c8c175d40dc9713af5a3a94b167abf4b5
ValeriyaKozlova <kozlova.valeriya.v@gmail.com>
2021-10-21 17:19:20 +0200

    types in typescript

---
+++ types.js
@@ -0,0 +1,30 @@
+var isFetching = true;
+var isLoading = false;
+var int = 42;
+var float = 4.2;
+var num = 3e10;
+var message = 'Hello Typescript';
+var numberArray = [1, 2, 3, 5, 8, 13];
+var numberArray2 = [1, 1, 1, 2, 4];
+var word = ['Hello', "Typescript"];
+// Tuple
+var contact = ['tel', 123456789];
+// Any
+var variable = 412;
+variable = [];
+// ===
+function syMyName(name) {
+    console.log(name);
+}
+syMyName('Valeriya');
+// Never
+function throwError(message) {
+    throw new Error(message);
+}
+function infinite() {
+    while (true) {
+    }
+}
+var login = 'admin';
+var id1 = 1234;
+var id2 = 'id';
