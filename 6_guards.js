ea4a3704244297e738eacb8554ef98ff31733da3
ValeriyaKozlova <kozlova.valeriya.v@gmail.com>
2021-10-22 16:41:51 +0200

    guards

---
+++ 6_guards.js
@@ -0,0 +1,36 @@
+function srip(x) {
+    if (typeof x === 'number') {
+        return x.toFixed(2);
+    }
+    return x.trim();
+}
+class MyResponse {
+    constructor() {
+        this.header = 'response header';
+        this.result = 'response result';
+    }
+}
+class MyError {
+    constructor() {
+        this.header = 'eror header';
+        this.message = 'response result';
+    }
+}
+function handle(res) {
+    if (res instanceof MyResponse) {
+        return {
+            info: res.header + res.result
+        };
+    }
+    else {
+        return {
+            info: res.header + res.message
+        };
+    }
+}
+function setAlertType(type) {
+    // ...
+}
+setAlertType('succes');
+setAlertType('danger');
+// setAlertType('default')
